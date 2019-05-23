3bbd3e9a9e93396b7a9dd800259108bca85adc93
Dhouglas Saraiva <48390877+dhougsaraiva@users.noreply.github.com>
2019-05-23 02:05:34 -0300

    Add files via upload

--- 2019.01_ContaBancaria-Jogo-Time_WS-DAO/src/br/mackenzie/projeto/time/TimeResource.java
+++ 2019.01_ContaBancaria-Jogo-Time_WS-DAO/src/br/mackenzie/projeto/time/TimeResource.java
@@ -45,6 +45,12 @@ public class TimeResource {
         return dao.lerTodos();
     }
     
+    @GET
+    @Path("/{id}")
+    public Time readID(@PathParam("id") LongParam idParam) {
+        return dao.lerPorID(idParam.get());
+    }
+    
     @PUT
     @Path("{id}")
     public Response update(@PathParam("id") LongParam idParam, Time time) {
